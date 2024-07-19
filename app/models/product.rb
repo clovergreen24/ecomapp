a381e5fe23c3efa3ed25c2d7e4ef69293b2f2406
Camilo Cobeaga <cobeagac@gmail.com>
2024-07-19 02:58:36 -0300

    added stocks to the products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
     has_one_attached :image do |attachable|
         attachable.variant :thumb, resize_to_limit: [100,100]
     end
+
+    has_many :stocks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :products
+    resources :products do
+      resources :stocks
+    end
   end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
