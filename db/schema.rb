43d6514cb170aeeaa2b35bd61896c498a536d71c
Camilo Cobeaga <cobeagac@gmail.com>
2024-07-28 21:22:01 -0300

    added categories to the products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_19_194905) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_29_000156) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_19_194905) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "stocks", force: :cascade do |t|
